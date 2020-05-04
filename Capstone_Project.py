9ba3e38ff85b996e68dff32e8d95a925cf0710c7
unknown <vvpandu18@gmail.com>
2020-05-04 17:46:56 -0500

    committing Capstone project files

---
+++ Capstone_Project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Notebook will be mainly used for the capstone project.
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
